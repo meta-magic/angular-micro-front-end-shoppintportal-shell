92b8a7b75d55cef4e0cfc1d3b91cf94d2c860748
datsgawas <dattaram.gawas@metamagic.in>
2019-08-13 15:31:28 +0530

    add user service integrated

--- src/app/constant/service.constant.ts
+++ src/app/constant/service.constant.ts
@@ -3,6 +3,7 @@
  */
 export const BASE_URl = 'http://35.227.30.18/';
 export const SERVICE_URL = {
-  AUTH : BASE_URl + 'authms/api/user/auth'
+  AUTH : BASE_URl + 'authms/api/user/auth',
+  ADD_USER : BASE_URl + 'authms/api/user/'
 };
 
